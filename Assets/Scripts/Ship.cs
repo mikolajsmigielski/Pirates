a869a7052e7961af7d9bff4cd8db47c2766d1a05
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-16 15:36:16 +0200

    The ability to move forward with linear acceleration of the ship has been added

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,27 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float MinSpeed = 2f;
+    public float MaxSpeed = 10f;
+    float CurrentSpeed; 
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        var TargetSpeed = MinSpeed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            TargetSpeed = MaxSpeed;
+        }
+        CurrentSpeed = Mathf.Lerp(CurrentSpeed, TargetSpeed, Time.deltaTime); 
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.forward * CurrentSpeed;
+    }
+}
