0d151df1ef0efbc4169fb6d8d29c0ab837bd4af8
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-18 16:09:57 +0200

    Update Camera.cs

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,13 +6,19 @@ public class Camera : MonoBehaviour
 {
     public Transform ObjectToTrack;
     public Vector3 Delta = new Vector3(0, 20, -40);
+    float cameraAngle = 0f;
     void Start()
     {
         
     } 
     void Update()
     {
-        var NewCameraposition = ObjectToTrack.position + Delta;
+        cameraAngle += Input.GetAxis("Mouse X");
+        var quaternion = Quaternion.Euler(0, cameraAngle, 0);
+
+        var NewCameraposition = ObjectToTrack.position + ObjectToTrack.rotation * quaternion * Delta;
         transform.position = NewCameraposition;
+        transform.LookAt(ObjectToTrack);
+        transform.rotation *= Quaternion.Euler(-10f, 0, 0);
     }
 }
