226b5eb3134656b96c86e28a8c7c674a03378dde
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-18 15:32:55 +0200

    Improved Ship movment to be more realistic

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -9,6 +9,7 @@ public class Ship : MonoBehaviour
     float CurrentSpeed;
     public float MaxAngle = 30;
     float CurrentAngle = 0;
+    public Transform ShipModelTransform;
     void Start()
     {
         
@@ -37,7 +38,15 @@ public class Ship : MonoBehaviour
         //movment
         var rigidbody = GetComponent<Rigidbody>();
         rigidbody.rotation *= Quaternion.Euler(Vector3.up * CurrentAngle * Time.deltaTime);
-        rigidbody.velocity =  rigidbody.rotation * Vector3.back * CurrentSpeed;
+        rigidbody.velocity =  rigidbody.rotation * Vector3.forward * CurrentSpeed;
+
+
+        //models rotation
+
+
+        var rotationX = Mathf.Sin(Time.timeSinceLevelLoad * 1.5f) * 2f;
+        var rotationZ = -CurrentAngle / 2;
+        ShipModelTransform.localRotation = Quaternion.Euler(rotationX, 180, rotationZ);
         
     }
 }
