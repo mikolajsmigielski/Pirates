94bfbd4d9d2161d8db859b04c3240a3c8206a2d2
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-19 21:01:46 +0200

    Add Particule system for water and terrain hit and script for destroying cannon ball after hit and some time

---
+++ Assets/Scripts/BoomScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomScript : MonoBehaviour
+{
+    public GameObject WaterPS;
+    public GameObject TerrainPS;
+    void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        var layerID = collision.collider.gameObject.layer;
+        var layerName = LayerMask.LayerToName(layerID);
+        
+        GameObject particlesObject = null;
+        if (layerName == "Water")
+            particlesObject = WaterPS;
+        if (layerName == "Terrain")
+            particlesObject = TerrainPS;
+
+
+        var position = collision.contacts[0].point;
+
+        Instantiate(particlesObject, position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
